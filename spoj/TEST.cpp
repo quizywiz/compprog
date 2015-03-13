ee56659cf69b4df5932158a0be11ca808b5da83c
quizywiz <kailash195@gmail.com>
2015-03-13 18:57:12 +0530

    Update TEST.cpp

---
+++ spoj/TEST.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num;
+	while(1)
+	{
+		scanf("%d",&num);
+		if(num==42)
+		return 0;
+		printf("%d\n",num);
+	}
+	return 0;
+}
+
+
